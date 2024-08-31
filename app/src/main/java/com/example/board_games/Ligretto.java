e22c7d11e89d025467ad0d3080a151a7cad12192
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2024-08-31 17:00:04 +0100

    Ligretto calculator running well

---
+++ app/src/main/java/com/example/board_games/Ligretto.java
@@ -0,0 +1,130 @@
+package com.example.board_games;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Ligretto extends AppCompatActivity {
+
+    int playersNum, registedPlayers, sent = 0;
+    ArrayList<String> playersNames = new ArrayList<String>();
+    ArrayList<Integer> playersPoints = new ArrayList<Integer>();
+
+    LinearLayout numPlayer, namePlayer, playerStatus,calculator;
+    EditText playerQuantity, playerName, negPoints, posiPoints;
+    Button checkNum, register, calculateButton, sendPoints;
+    TextView name, points, playerSendName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ligretto);
+
+        playerStatus = findViewById(R.id.player_status);
+        numPlayer = findViewById(R.id.layout_numPlayers);
+        namePlayer = findViewById(R.id.layout_player_register);
+        playerQuantity = findViewById(R.id.player_number);
+        playerName = findViewById(R.id.player_name);
+        checkNum = findViewById(R.id.check_num_players);
+        register = findViewById(R.id.register_button);
+        calculateButton = findViewById(R.id.calculate_button);
+        sendPoints = findViewById(R.id.sendButton);
+        negPoints = findViewById(R.id.negative_points);
+        posiPoints = findViewById(R.id.positive_points);
+        playerSendName = findViewById(R.id.player_name_to_send);
+        name = findViewById(R.id.player_name_apresentation);
+        points = findViewById(R.id.player_points_apresentation);
+        calculator = findViewById(R.id.layout_points_calculator);
+
+        newGame();
+
+        checkNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playersNum = Integer.parseInt(playerQuantity.getText().toString());
+                namePlayer.setVisibility(View.VISIBLE);
+                numPlayer.setVisibility(View.GONE);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playersNames.add(playerName.getText().toString());
+                playersPoints.add(0);
+                playerName.getText().clear();
+                registedPlayers++;
+
+                if(registedPlayers == playersNum)
+                {
+                    namePlayer.setVisibility(View.GONE);
+                    playerStatus.setVisibility(View.VISIBLE);
+                    calculateStatus();
+                }
+            }
+        });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerSendName.setText(playersNames.get(sent));
+                calculator.setVisibility(View.VISIBLE);
+            }
+        });
+
+        sendPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+            calculate(sent, Integer.parseInt(negPoints.getText().toString()), Integer.parseInt(posiPoints.getText().toString()));
+            sent++;
+            posiPoints.getText().clear();
+            negPoints.getText().clear();
+            if(sent == playersNames.size())
+            {
+                calculator.setVisibility(View.GONE);
+                sent = 0;
+            }
+            else
+            {
+                playerSendName.setText(playersNames.get(sent));
+            }
+            calculateStatus();
+            }
+        });
+    }
+
+    public void newGame(){
+        playersNum = 2;
+        registedPlayers = 0;
+        numPlayer.setVisibility(View.VISIBLE);
+        namePlayer.setVisibility(View.GONE);
+        playerStatus.setVisibility(View.GONE);
+        calculator.setVisibility(View.GONE);
+    }
+
+    public void calculateStatus(){
+        String tempNames = "", tempPoints = "";
+        for(int i = 0; i<playersNum; i++){
+            tempNames += playersNames.get(i) + "\n";
+            tempPoints +=playersPoints.get(i) + "\n";
+        }
+        name.setText(tempNames);
+        points.setText(tempPoints);
+    }
+
+    public void calculate(int index, int pointsSubtractor, int pointsPositives)
+    {
+        int newPoints = (playersPoints.get(index) - (2 * pointsSubtractor) + pointsPositives);
+        playersPoints.set(index, newPoints);
+    }
+
+}
---
+++ app/src/main/java/com/example/board_games/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.board_games;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ligrettoGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ligrettoGame = findViewById(R.id.ligreto);
+
+        ligrettoGame.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent LigretoNewGame = new Intent(MainActivity.this, Ligretto.class);
+                startActivity(LigretoNewGame);
+            }
+        });
+
+    }
+
+}
